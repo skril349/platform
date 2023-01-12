0ac6e68698ec0f2148bf475ef8c09cabe5bbd698
toniprotofy <toni@protofy.com>
2023-01-12 20:06:58 +0100

    creating docker-compose with mongo and strapi server

---
+++ server/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd23f85116ef005a1eba5f9eb798c1588'),
+    },
+  },
+});
---
+++ server/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9b774c3e-2aaa-4c09-a24f-53550d4fd1b2'
+};
